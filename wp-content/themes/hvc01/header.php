0b9ba71ad47d5f542235ac76eb52c7790cf5e15c
paranormallife <elliottoman@gmail.com>
2019-05-30 20:18:34 -0400

    GSuite Meta

--- wp-content/themes/hvc01/header.php
+++ wp-content/themes/hvc01/header.php
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html, charset=UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta name="google-site-verification" content="oTg9eTd1PXbsyS1QUCkHxLWqBY1L42uWm0YLhhbBRrY" />
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136105276-2"></script>
